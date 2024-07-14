f196e31beff5754eb53721299b8927f263d2d8b2
KhangLe1412 <lekhang.1412@gmail.com>
2024-07-14 15:40:28 +0700

    init project

---
+++ index.js
@@ -0,0 +1,11 @@
+function getValue() {
+    // Get the input element
+    var input = document.getElementById("search-text");
+  
+    // Get the value of the input
+    var value = input.value;
+  
+    // Display the value
+    console.log(value);
+    // or you can use alert(value) to display it in an alert box
+  }
